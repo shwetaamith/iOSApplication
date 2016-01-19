e93219f19e2214961d608245627bdfd86541611d
smurthy3 <smurthy3@prlta213macc19.care.asu.edu>
2016-01-19 16:55:16 -0700

    Initial Commit

---
+++ MovieDescriptor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDescriptor
+//
+//  Created by smurthy3 on 1/19/16.
+//  Copyright © 2016 edu.asu.msse.smurthy3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
